53eeb309f7f895e266a2acbd962b1518c37d4163
aaroncarver <agcarvr@gmail.com>
2021-05-20 12:26:29 -0600

    comment removed

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    connectTimeoutMS: 3000  //<--ADD THIS
+    connectTimeoutMS: 3000
 },(err, res)=>{
     try{
         console.log("Mongo is Connected")
