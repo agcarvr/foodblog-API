929228d0b1c66abe33275c299f5468be1284ed07
aaroncarver <agcarvr@gmail.com>
2021-05-20 10:20:06 -0600

    pushing all commits

--- models/Post.js
+++ models/Post.js
@@ -4,7 +4,8 @@ const postSchema = new Schema({
     title: { type: String, required: true, unique: true },
     body: String,
     dish: { type: String, required: true },
-    cuisine: { type: String, required: true }
+    cuisine: { type: String, required: true },
+    summary: String
 })
 
 module.exports = model('Post', postSchema);
